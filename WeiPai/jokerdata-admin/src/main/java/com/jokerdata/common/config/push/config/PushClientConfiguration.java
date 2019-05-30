0a280673bbcebbe45b07111c0dc6a8e36f6f09cf
oldma <1013789753@qq.com>
2019-05-30 16:35:41 +0800

    极光

---
+++ WeiPai/jokerdata-admin/src/main/java/com/jokerdata/common/config/push/config/PushClientConfiguration.java
@@ -0,0 +1,39 @@
+package com.jokerdata.common.config.push.config;
+
+import cn.jpush.api.JPushClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 消息推送
+ * @author :zhuys
+ * @date: 2019/4/27 15:11
+ */
+@Configuration
+public class PushClientConfiguration {
+
+    Logger logger = LoggerFactory.getLogger(PushClientConfiguration.class);
+
+    @Value("${jpush.MasterSecret}")
+    private String masterDriverSecret;
+
+    @Value("${jpush.AppKey}")
+    private String driverAppKey;
+
+
+
+    /**
+     * 司机端推送
+     * @return
+     */
+    @Bean("pushDriverClient")
+    public JPushClient pushDriverClientFactory() {
+        logger.info("司机端Push注入成功！！");
+        return new JPushClient(masterDriverSecret, driverAppKey);
+    }
+
+
+}
